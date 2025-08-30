555ece19e5c9aac7f35859a6ed7c88f6a3c2a792
Mike The Madkiwi <mike@hildyard.ca>
2025-08-30 05:17:03 -0400

    Update nohelmet.lua

--- nohelmet.lua
+++ nohelmet.lua
@@ -1,1 +1,13 @@
+Citizen.CreateThread( function()
 
+	while true do
+		Citizen.Wait(0)		
+		local playerPed = GetPlayerPed(-1)
+		local playerVeh = GetVehiclePedIsUsing(playerPed)
+		SetPedConfigFlag(playerPed, 35, true) -- do not use helmets
+		SetPedConfigFlag(playerPed, 380, true) -- do not autoenable helmets		
+		if playerVeh ~= 0 then RemovePedHelmet(playerPed,true) end -- remove helmet if i am trying to wear one, or if i already am
+    -- ^^ this line will remove helmets ALWAYS. regardless of if it is wanted, one of your devs will have to put it in as a choice not forced. 
+	end
+	
+end)
